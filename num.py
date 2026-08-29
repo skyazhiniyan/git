3faa0e2156bfddca4e26d5e2f1c033f196bc8bdc
yaazhiniyan <s.k.yazhiniyan@gmail.com>
2026-08-29 10:37:59 +0530

    new number gussing game

--- num.py
+++ num.py
@@ -1,11 +1,23 @@
-print("hello world")
-user = input("enter game :")
-if user == "game":
-    print("start game ")
-else :
-    print("no game")
-
-
+import random
+print("welcome to the number guessing game")
+while True:
+    computer = random.randint(1,5)  
+    user_input = input("enter your correct number: ")
+    if user_input == "show the number":
+        print(f"The number is: {computer}")
+    else:
+        try:
+            user = int(user_input)
+            if user == computer:
+                print("you are correct")
+                break
+            elif user > computer:
+                print("try a lower number")
+            elif user < computer:
+                print("try a higher number")
+        except ValueError:
+            print("invalid input, please enter a number or 'show the number'")
+            break
 
 
 
